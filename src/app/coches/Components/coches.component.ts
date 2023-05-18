b13b3a7b900c39a56127b2f2d6033140395d233f
Antonio Martínez González <antonio.martinez.gonzalez.1990@gmail.com>
2023-05-18 15:11:56 +0200

    cambios windows

--- src/app/coches/Components/coches.component.ts
+++ src/app/coches/Components/coches.component.ts
@@ -17,10 +17,7 @@ export class CochesComponent {
       precio:40000
     },{
       name:'Mini',
-<<<<<<< HEAD
-=======
       modelo:'Cooper',
->>>>>>> 944f9a8d1af24bb904b3257fee88d8e232576f23
       precio:35000
     }];
 }
